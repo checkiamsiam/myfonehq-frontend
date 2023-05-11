54ffcaae6a5e3f13a0cf1f6c10e066f0d5622493
itsproali <itsproali@gmail.com>
2023-05-11 13:39:08 +0600

    Footer Links Updates

--- src/components/layout/footer/widgets.tsx
+++ src/components/layout/footer/widgets.tsx
@@ -1,17 +1,17 @@
-import Container from '@components/ui/container'
-import WidgetLink from '@components/widgets/widget-link'
-import cn from 'classnames'
+import Container from "@components/ui/container";
+import WidgetLink from "@components/widgets/widget-link";
+import cn from "classnames";
 
 interface WidgetsProps {
   widgets: {
-    id: number
-    widgetTitle?: string
-    lists: any
-    isCompanyIntroduction?: boolean
-    logo?: any
-  }[]
+    id: number;
+    widgetTitle?: string;
+    lists: any;
+    isCompanyIntroduction?: boolean;
+    logo?: any;
+  }[];
 
-  variant?: 'contemporary'
+  variant?: "contemporary";
 }
 
 const Widgets: React.FC<WidgetsProps> = ({ widgets, variant }) => {
@@ -19,10 +19,10 @@ const Widgets: React.FC<WidgetsProps> = ({ widgets, variant }) => {
     <Container>
       <div
         className={cn(
-          'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-9 xl:gap-5  pb-9 md:pb-14 lg:pb-16 2xl:pb-20 3xl:pb-24 lg:mb-0.5 2xl:mb-0 3xl:-mb-1',
+          "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-9 xl:gap-5  pb-9 md:pb-14 lg:pb-16 2xl:pb-20 3xl:pb-24 lg:mb-0.5 2xl:mb-0 3xl:-mb-1",
           {
-            'xl:grid-cols-6': variant !== 'contemporary',
-            'xl:grid-cols-7': variant === 'contemporary',
+            "xl:grid-cols-6": variant !== "contemporary",
+            "xl:grid-cols-7": variant === "contemporary",
           }
         )}
       >
@@ -30,13 +30,13 @@ const Widgets: React.FC<WidgetsProps> = ({ widgets, variant }) => {
           <WidgetLink
             key={`footer-widget--key${widget.id}`}
             data={widget}
-            className='pb-3 md:pb-0'
-            variant='contemporary'
+            className="pb-3 md:pb-0"
+            variant="contemporary"
           />
         ))}
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Widgets
+export default Widgets;
